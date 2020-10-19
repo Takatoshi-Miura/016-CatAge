7bb3f78149507c3066ee0b7aca08d5ff768fb551
Takatoshi Miura <takatoshi052031@gmail.com>
2020-10-20 08:46:02 +0900

    [Update] ViewControllerにて、猫年齢を人間年齢へ換算し、結果をラベルに表示する処理を実装。

--- 016-CatAge/ViewController.m
+++ 016-CatAge/ViewController.m
@@ -9,14 +9,53 @@
 
 @interface ViewController ()
 
+// UIの設定
+@property (weak, nonatomic) IBOutlet UITextField *catAgeField;
+@property (weak, nonatomic) IBOutlet UILabel *humanAgeLabel;
+- (IBAction)calcButton:(id)sender;
+
+// メソッドの宣言
+- (double)catAgeToHumanAge:(double)catAge;
+
 @end
 
 @implementation ViewController
 
+// ライフサイクルメソッド
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
+// 計算ボタンの処理
+- (IBAction)calcButton:(id)sender {
+    // 猫年齢計算
+    double catAge = [_catAgeField.text doubleValue];
+    double humanAge = [self catAgeToHumanAge:catAge];
+    
+    // ラベル表示
+    NSString *ans;
+    ans = [NSString stringWithFormat:@"%.1f歳",humanAge];
+    _humanAgeLabel.text = ans;
+}
+
+// 猫年齢計算メソッド
+- (double)catAgeToHumanAge:(double)catAge {
+    double age;
+    
+    if (catAge <= 1) {
+        age = 20*catAge;
+    } else if (catAge <= 5) {
+        age = catAge*6 + 15;
+    } else if (catAge <= 10) {
+        age = catAge*5 + 20;
+    } else if (catAge <= 15) {
+        age = catAge*4 + 30;
+    } else {
+        age = catAge*3 + 45;
+    }
+    
+    return age;
+}
 
 @end
